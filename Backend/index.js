4d3c3fa3645f28444a998ad0549d15221364a4e6
sinans <svrsinan06@gmail.com>
2023-12-20 23:28:39 +0300

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+//express
+const express= require("express")
+const app=express()
+
+//.env 
+require("dotenv").config();
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || 8000;
+
+//async-errors
+require("express-async-errors")
+
+//DB Connection
+const {dbConnection} = require("./src/configs/dbConnection")
+dbConnection()
+
+//JSON data
+app.use(express.json())
+
+//Find/Pagination
+app.use(require('./src/middlewares/findSearchSortPage'))
+
+//Home Page
+app.all("/",(req,res)=>{
+
+    res.send({
+        error:false,
+        message:"Welcome to NoteApp API!"
+    })
+
+})
+
+//errorHandler
+app.use(require("./src/middlewares/errorHandler"))
+
+//Listening PORT
+app.listen(PORT,()=>console.log(`Running at: http://${HOST}:${PORT}`))
